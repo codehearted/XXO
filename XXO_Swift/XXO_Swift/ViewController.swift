7619d38a7bf1a1cc419b081a3cdf417cc4f089d1
Paul Jacobs <paul.jacobs@gmail.com>
2015-01-25 09:36:50 -0800

    Created new project using Single View Swift Template.

---
+++ XXO_Swift/XXO_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XXO_Swift
+//
+//  Created by Paul Jacobs on 1/25/15.
+//  Copyright (c) 2015 Paul Jacobs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
